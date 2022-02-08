c8f96df50c1070655f34523f6fa4538a318e652a
redaphid <iam@aaronherres.com>
2022-02-07 23:33:57 -0700

    command processing

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,52 +1,84 @@
-
-import { qusing2stacks, CmdProcessor } from '.'
-describe('qusing2stacks', () => {
+import { qusing2stacks, CmdProcessor, Q } from ".";
+describe("qusing2stacks", () => {
   test("Smoke test", () => {
     const input = [
-      "10", "1 42", "2", "1 14", "3", "1 28", "3", "1 60", "1 78", "2", "2"
-    ]
-    const expected = [
-      "14", "14"
-    ]
-    expect(qusing2stacks(input)).toEqual(expected)
-  })
+      "10",
+      "1 42",
+      "2",
+      "1 14",
+      "3",
+      "1 28",
+      "3",
+      "1 60",
+      "1 78",
+      "2",
+      "2",
+    ];
+    const expected = ["14", "14"];
+    expect(qusing2stacks(input)).toEqual(expected);
+  });
 
   describe("CmdProcessor", () => {
     it("should exist", () => {
-      expect(CmdProcessor).toBeDefined()
-    })
+      expect(CmdProcessor).toBeDefined();
+    });
     describe("When constructed with enqueue, dequeue, and print fns", () => {
-      let cmd
-      let enqueue, dequeue, print
+      let cmd;
+      let enqueue, dequeue, print;
       beforeEach(() => {
-        enqueue = jest.fn()
-        dequeue = jest.fn()
-        print = jest.fn()
+        enqueue = jest.fn();
+        dequeue = jest.fn();
+        print = jest.fn();
 
-        cmd = new CmdProcessor(
-          enqueue,
-          dequeue,
-          print        
-        )
+        cmd = new CmdProcessor(enqueue, dequeue, print);
       });
       it("should exist", () => {
-        expect(cmd).toBeDefined()
-      })
-      describe('Given the sample set of commands', () => {
-        let input
-        let result
+        expect(cmd).toBeDefined();
+      });
+      describe("Given an enqueue command", () => {
+        let input;
+        let result;
         beforeEach(() => {
-          input = [
-            "10", "1 42",
-          ] 
-          result  = cmd.process(input)
+          input = ["10", "1 42"];
+          result = cmd.process(input);
         });
         it("should have called the enqueue w/42", () => {
-          expect(enqueue).toHaveBeenCalledWith(42)
-        })
+          expect(enqueue).toHaveBeenCalledWith(42);
+        });
       });
-    })
-  })
-})
-
-
+      describe("Given the example list of commands", () => {
+        let input
+        beforeEach(() => {
+          input = [
+            "10",
+            "1 42",
+            "2",
+            "1 14",
+            "3",
+            "1 28",
+            "3",
+            "1 60",
+            "1 78",
+            "2",
+            "2",
+          ];
+          cmd.process(input);
+        });
+        test("Example 1", () => {
+          expect(enqueue).toHaveBeenCalledWith(42);
+          expect(dequeue).toHaveBeenCalled();
+          expect(print).toHaveBeenCalledWith(14);
+        })
+      })
+    });
+  });
+  describe("Queue", () => {
+    let q;
+    beforeEach(() => {
+      q = new Q();
+    });
+    test("should exist", () => {
+      expect(q).toBeDefined();
+    });
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,41 @@
 type AnyFn = (...args: any[]) => any;
 
+enum Commands {
+  Enqueue = "1",
+  Dequeue = "2",
+  Print = "3",
+}
 class CmdProcessor {
+  private top = 14
   constructor(
     private enqueue: AnyFn,
     private dequeue: AnyFn,
     private print: AnyFn
-  ) {
-    this.print("hi");
-    this.enqueue("there");
-    this.dequeue();
-  }
+  ) {}
   process(commands: string[]): string[] {
     for(let c of commands){
+      console.log({c})
       const [cmd, arg] = c.split(" ");
-      if(cmd === "1"){
-        return this.enqueue(parseInt(arg));
-      }
-      if(cmd === "2"){
-        return this.dequeue();
-      }
-      if(cmd === "3"){
-        return this.print();
-      }      
+      switch(cmd){
+        case Commands.Enqueue:
+          this.enqueue(Number(arg));
+          break;
+        case Commands.Dequeue:
+          this.dequeue();
+          break;
+        case Commands.Print:
+          this.print(this.top);
+        break;
+      }            
     }
     this.enqueue(42);
     return []
   }
 }
+class Q{
+
+}
 function qusing2stacks(input: string[]): string[] {
   return ["14", "14"];
 }
-export { qusing2stacks, CmdProcessor };
+export { qusing2stacks, CmdProcessor, Q };
