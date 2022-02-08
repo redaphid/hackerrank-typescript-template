a66eecfc6b679396a6c91569d19676580bf3204f
redaphid <iam@aaronherres.com>
2022-02-07 23:19:34 -0700

    command parser

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,19 @@ class CmdProcessor {
     this.enqueue("there");
     this.dequeue();
   }
-  process(cmd: string[]): string[] {
+  process(commands: string[]): string[] {
+    for(let c of commands){
+      const [cmd, arg] = c.split(" ");
+      if(cmd === "1"){
+        return this.enqueue(parseInt(arg));
+      }
+      if(cmd === "2"){
+        return this.dequeue();
+      }
+      if(cmd === "3"){
+        return this.print();
+      }      
+    }
     this.enqueue(42);
     return []
   }
