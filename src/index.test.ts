eb5312e1bea18130561536b0b30c4b89337ed425
redaphid <iam@aaronherres.com>
2022-02-08 21:27:10 -0700

    copilot just taught me about spyOn!

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,28 @@
-import {whatever} from './index';
-  test("Let's get things started", ()=>{
-    expect(whatever).not.toBeDefined();
-  })
-
+import { editor, Executor } from "./index";
+test("Let's get things started", () => {
+  expect(editor).toBeDefined();
+});
+describe("Executor", () => {
+  it("should exist", () => {
+    expect(Executor).toBeDefined();
+  });
+  describe("Given an Executor", () => {
+    let parser;
+    //create a command parser, then spy on it
+    beforeEach(() => {
+      parser = new Executor();
+      jest.spyOn(parser, "append");
+      jest.spyOn(parser, "print");
+      jest.spyOn(parser, "undo");
+    });
+    describe("When executing a command that appends 'Aaron'", () => {
+      let result
+      beforeEach(() => {
+        result = parser.execute("1 Aaron");
+      });
+    });
+    it("should call the correct functions", () => {
+      expect(parser.append).toHaveBeenCalledWith("Aaron");     
+    });
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-function whatever(){}
+function editor() {}
 
-export {whatever};
+class Executor {
+  append(s: string) {}
+  print(i: number) {}
+  undo() {}
+  execute(s: string[]) {
+    return [];
+  }
+}
+export { editor, Executor };
