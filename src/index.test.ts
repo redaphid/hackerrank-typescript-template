3b62619e24608885df67ed39cef4752f2c9f99aa
redaphid <iam@aaronherres.com>
2022-02-08 21:45:23 -0700

    parser switch tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -6,23 +6,53 @@ describe("Executor", () => {
   it("should exist", () => {
     expect(Executor).toBeDefined();
   });
-  describe("Given an Executor", () => {
-    let parser;
+  describe("Command Parsing", () => {
+    let exe;
     //create a command parser, then spy on it
     beforeEach(() => {
-      parser = new Executor();
-      jest.spyOn(parser, "append");
-      jest.spyOn(parser, "print");
-      jest.spyOn(parser, "undo");
+      exe = new Executor();
+      jest.spyOn(exe, "append");
+      jest.spyOn(exe, "delete");
+      jest.spyOn(exe, "print");
+      jest.spyOn(exe, "undo");
     });
     describe("When executing a command that appends 'Aaron'", () => {
-      let result
+      let result;
       beforeEach(() => {
-        result = parser.execute("1 Aaron");
+        result = exe.execute("1 Aaron");
       });
       it("should call the correct functions", () => {
-        expect(parser.append).toHaveBeenCalledWith("Aaron");     
+        expect(exe.append).toHaveBeenCalledWith("Aaron");
       });
     });
+    describe("when executing the delete command", () => {
+      let result;
+      beforeEach(() => {
+        result = exe.execute("2 10");
+      });
+      it("should call the correct functions", () => {
+        expect(exe.delete).toHaveBeenCalledWith(10);
+      });
+    })
+  });
+  describe("When executing a adds a silly string, then prints a char at it's index", () => {
+    let exe
+    let result;
+    beforeEach(() => {
+      exe = new Executor();
+      exe.append("donovan");
+      result = exe.print(1);
+    });
+    it("should call the correct functions", () => {
+      expect(result).toEqual("o");
+    });
+    describe("When deleting the last characer", () => {
+      beforeEach(() => {
+        exe.undo();
+      });
+      it("should call the correct functions", () => {
+        expect(exe.buf).toEqual("donovan");
+      });
+    })
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,32 @@
 function editor() {}
 
 class Executor {
-  append(s: string) {}
-  print(i: number) {}
+  buf: string = '';
+  append(s: string) {
+    this.buf+=s
+  }
+  print(i: number) {
+    return this.buf[i]
+  }
+  delete(i:number) {}
   undo() {}
   execute(s: string) {
     const [command, ...args] = s.split(" ");
     switch (command) {
       case "1":
         this.append(args[0]);
-        return
+        return;
       case "2":
-        this.print(parseInt(args[0]));
+        this.delete(parseInt(args[0]));
         return
       case "3":
+        this.print(parseInt(args[0]));
+        return
+      case "4":
         this.undo();
         return
       default:
-        throw new Error("Invalid command");
-        return
+        throw new Error("Invalid command");        
     }
   }
 }
