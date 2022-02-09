18b675dea201658e035eeed044f269e7f551a4a0
redaphid <iam@aaronherres.com>
2022-02-08 20:53:41 -0700

    Done in less than half the allotted time. Copilot helped a lot, but I still needed to think about it and tweak the code

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,13 @@
 import {pairs} from './index';
   test("Example 1", ()=>{
-    const input = [1,2,3,4]
+    const nums = [1,2,3,4]
     const diff =1
-    expect(pairs(input, diff)).toEqual(3)
+    expect(pairs(diff, nums)).toEqual(3)
+  })
+  test("When we ask for a difference of 2, it should return 1", ()=>{
+    const nums = [1,2,3,4]
+
+    const diff = 2
+    expect(pairs(diff, nums)).toEqual(2)
   })
 
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
-function pairs(n:number[], diff:number):number {
-  return 3
+function pairs(target:number, pairs:number[]):number {
+  let count = 0;
+  for (let i = 0; i < pairs.length; i++) {
+    const n1 = pairs[i];
+    for (let j = i + 1; j < pairs.length; j++) {
+      const n2 = pairs[j];
+      const diff = Math.abs(n1 - n2);
+      if (diff === target) {
+        // console.log({i, n1, n2, diff});
+        count++;
+      }
+    }
+  }
+  return count;
+  // return 3
 }
 export {pairs};
