efa63a63d3ca90d31259b3cd7f0d96ddaaef9ed1
redaphid <iam@aaronherres.com>
2022-02-08 18:04:47 -0700

    basics checking opening/closing

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,20 @@
-import {whatever} from './index';
-  test("Let's get things started", ()=>{
-    expect(whatever).not.toBeDefined();
+import { balanced } from './index';
+describe("balanced", ()=>{
+  test("Let's get things started", () => {
+    expect(balanced).toBeDefined();
+  })
+  test("{} should return true", () => {
+    const result = balanced("{}")
+    expect(result).toBe(true)
+  })
+  test("{ should return false", () => {
+    const result = balanced('{')
+    expect(result).toBe(false)
   })
 
+  test("}} should return false", ()=>{
+    const result = balanced("{[}]")
+    expect(result).toBe(false)
+  })
+  
+})
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,29 @@
-function whatever(){}
+import _ from "lodash";
+const openersToClosers = {
+  "{": "}",
+  "[": "]",
+};
+const openers = _.keys(openersToClosers);
+const closers = _.values(openersToClosers);
+function balanced(s: string): boolean {
+  if (s.length % 2 !== 0) return false;
+  
+  const iStack = s.split("");
+  const oStack: string[] = [];  
 
-export {whatever};
+  while (iStack.length > 0) {
+    const c = iStack.shift();
+    if (!c) return true;
+    console.log({c})
+    if (openers.includes(c)) {
+      oStack.push(c);
+      console.log("added opener", c)
+      continue;
+    }
+    if (oStack.length === 0) return false;
+    console.log(c);
+  }
+  return true;
+}
+
+export { balanced };
