f46ba01749f5bc999845dbdc034ce2171201fb90
redaphid <iam@aaronherres.com>
2022-02-08 21:12:22 -0700

    whelp, we got closer. But time's up

--- src/index.ts
+++ src/index.ts
@@ -2,39 +2,36 @@ import _ from "lodash";
 const closersToOpeners = {
   "}": "{",
   "]": "[",
-  ")":"(",
+  ")": "(",
 };
 const openers = _.values(closersToOpeners);
 const closers = _.keys(closersToOpeners);
 function balanced(s: string): boolean {
   if (s.length % 2 !== 0) return false;
-  
+
   const iStack = s.split("");
-  const oStack: string[] = [];  
+  const oStack: string[] = [];
 
   while (iStack.length > 0) {
-    const c = iStack.shift();
-    if (!c) return true;
+    const c = iStack.shift() || "";
+    if (!openers.includes(c) && !closers.includes(c)) return false;
 
-    //Check if the character is even valid
-    if(!openers.includes(c) && !closers.includes(c)) return false
-    
     if (openers.includes(c)) {
-      oStack.push(c)
-      continue;
+      if (oStack.length === 0) {
+        oStack.push(c);
+        continue;
+      }
+      const last = oStack.pop();
+      if (closersToOpeners[c] !== last) return false;
+      // oStack.pop();
     }
-
-    const last = oStack.pop()
-    if(!last) return false
-    const o = closersToOpeners[c]
-
- 
-    console.log({last,o,c})
-    if(o !== last) return false
+    if (closers.includes(c)) {
+      const last = oStack.pop();
+      if (last !== closersToOpeners[c]) {
+        return false;
+      }
+    }
   }
-  console.log("tail case")
-  console.log({oStack, iStack})
-  return oStack.length === 0;
+  return true;
 }
-
 export { balanced };
